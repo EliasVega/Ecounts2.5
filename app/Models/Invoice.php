3a619df27fe3f1130ab2b356ecd27413e3481f09
EliasVega <discom.is@gmail.com>
2023-02-28 00:05:14 -0500

    se repara show invoice

--- app/Models/Invoice.php
+++ app/Models/Invoice.php
@@ -30,7 +30,7 @@ class Invoice extends Model
     ];
 
     public function user(){
-        return $this->belongsTo(Customer::class);
+        return $this->belongsTo(User::class);
     }
 
     public function customer(){
