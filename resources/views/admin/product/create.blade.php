a0ce06f65713c5a9c052c5efa41cbd8026f76954
EliasVega <discom.is@gmail.com>
2023-07-05 15:47:40 -0500

    retroceder

--- resources/views/admin/product/create.blade.php
+++ resources/views/admin/product/create.blade.php
@@ -20,9 +20,64 @@
             @endif
             {!!Form::open(array('url'=>'product', 'method'=>'POST', 'autocomplete'=>'off', 'files' => 'true'))!!}
             {!!Form::token()!!}
-                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                    @include('admin/product.form')
+            <div class="box-body row">
+                <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+                    <div class="form-group">
+                        <label for="name">Nombre del product</label>
+                        <input type="text" name="name" value="{{ old('name') }}" class="form-control" placeholder="Nombre del producto">
+                    </div>
                 </div>
+                <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+                    <div class="form-group">
+                        <label for="category_id">Categorias</label>
+                        <div class="select">
+                            <select name="category_id" class="form-control selectpicker" data-live-search="true" id="category_id" required>
+                                <option value="" disabled selected>Seleccionar.</option>
+                                @foreach($categories as $cat)
+                                <option value="{{ $cat->id }}">{{ $cat->name }}</option>
+                                @endforeach
+                            </select>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+                    <div class="form-group">
+                        <label for="unit_measure_id">U/medida</label>
+                        <div class="select">
+                            <select name="unit_measure_id" class="form-control selectpicker" data-live-search="true" id="unit_measure_id" required>
+                                <option value="" disabled selected>Seleccionar.</option>
+                                @foreach($measures as $mea)
+                                <option value="{{ $mea->id }}">{{ $mea->name }}</option>
+                                @endforeach
+                            </select>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+                    <div class="form-group">
+                        <label for="code">Codigo</label>
+                        <input type="text" name="code" value="{{ old('code') }}" class="form-control" placeholder="Codigo del product">
+                    </div>
+                </div>
+                <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+                    <div class="form-group">
+                        <label for="price">Precio</label>
+                        <input type="text" name="price" value="{{ old('price') }}" class="form-control" placeholder="Precio de Compra">
+                    </div>
+                </div>
+                <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+                    <div class="form-group">
+                        <label for="image">Imagen</label>
+                        <input type="file" name="image" class="form-control" id="image">
+                    </div>
+                </div>
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                    <div class="form-group">
+                        <button class="btn btn-celeste btn-md" type="submit"><i class="fa fa-save"></i>&nbsp; Guardar</button>
+                        <a href="{{url('product')}}" class="btn btn-gris"><i class="fa fa-window-close"></i>&nbsp; Cancelar</a>
+                    </div>
+                </div>
+            </div>
             {!!Form::close()!!}
         </div>
     </div>
